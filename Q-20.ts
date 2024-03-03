780d2b8ddd4da7d5a12086dc55813e547910cb13
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:48:55 +0500

    Add files via upload

---
+++ Q-20.ts
@@ -0,0 +1,6 @@
+//20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let socialmediaPlatform : string[]=[ "WhatsApp","LinkedIn","facebook", "You Tube"]
+console.log("list of socialmediaPlatform")
+
+ socialmediaPlatform.map((items)=>console.log(`${items}`))
